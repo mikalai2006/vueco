8d690c060791a840f69f1867a2994a82706d0d9d
Mikalai Parakhnevich <composer.opencart@gmail.com>
2022-12-20 18:37:37 +0300

    Updates

---
+++ assets/config.27792f0c.js
@@ -0,0 +1,2 @@
+import{d as t,r as m,a as s}from"./decorateStory.fef6c3d8.js";import"./iframe.10fce278.js";import"./vue.esm-bundler.d6e8a8ce.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
+//# sourceMappingURL=config.27792f0c.js.map
